91d80629eb53a60aab39e591511513642468f9b1
qjlxg <qjlxg@163.com>
2024-11-26 20:58:07 +0800

    Update channel.php

--- channel.php
+++ channel.php
@@ -1,1 +1,9 @@
-channel.php
+<?php
+$telegramChannelURLs = [
+    "https://t.me/s/networknim",
+    "https://t.me/s/beiten",
+    "https://t.me/s/MsV2ray",
+    "https://t.me/s/foxrayiran",
+    "https://t.me/s/DailyV2RY",
+    ];
+?>
